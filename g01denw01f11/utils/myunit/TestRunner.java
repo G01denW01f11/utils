e0afe2c0e380ebb3fcd7883b4972c0441aa527a8
Jonathan E. T. Means <jetmeans@gmail.com>
2016-02-15 11:14:38 -0500

    Add colored output

--- g01denw01f11/utils/myunit/TestRunner.java
+++ g01denw01f11/utils/myunit/TestRunner.java
@@ -12,11 +12,22 @@ import java.util.Map;
  */
 public class TestRunner {
 
+    private List<TestCase> tests = new LinkedList<>();
+    private List<Method> testMethods = new LinkedList<>();
+    private Map<AssertionError, Method> failedTests = new HashMap<>();
+
+    //For color output
+    //This apparently doesn't work on Windows, but I don't care
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    //TODO: This looks yellow?
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     public void runTests() throws InvocationTargetException {
         //I can't imagine that runTests() will be called more than once during a single
         //execution of the program, but just to be safe...
         failedTests.clear();
-        for (TestCase test: tests) {
+        for (TestCase test : tests) {
             runTest(test);
         }
         printResults();
@@ -31,7 +42,7 @@ public class TestRunner {
         }
     }
 
-    private void runTest(TestCase test) throws InvocationTargetException {
+    private void setUp(TestCase test) throws InvocationTargetException {
         try {
             test.getClass().getMethod("setUp").invoke(test);
         } catch (IllegalAccessException e) {
@@ -40,54 +51,60 @@ public class TestRunner {
             //Not all com.g01denw01f11.utils.tests need to have a setUp() method
             //so just don't do anything
         }
+    }
 
-        for (Method method : test.getClass().getMethods()) {
-            if (method.getName().startsWith("test")) {
-                if (method.getName().contains("Throws")) {
-                    @SuppressWarnings("unchecked")
-                    Class<Throwable> err = (Class<Throwable>)method.getParameterTypes()[0];
-                    try {
-                        method.invoke(method.getDeclaringClass().newInstance(), err.newInstance());
-                    } catch (InvocationTargetException e) {
-                        //noinspection StatementWithEmptyBody
-                        if (e.getTargetException().getClass().equals(err)) {
-                            //test passes
-                        } else {
-                            failedTests.put(new AssertionError(err.getSimpleName() + " was not thrown"), method);
-                        }
-                    } catch (Exception e) {
-                        failedTests.put(new AssertionError(e.getClass().getSimpleName() + " thrown."), method);
-                    }
-                } else {
+    private void runTest(TestCase test) throws InvocationTargetException {
+        //This method can throw if the TestCase's setUp() method throws something
+        //If this happens, the tests will be meaningless, so we'll just
+        //throw everything back to the caller
+        setUp(test);
 
-                    try {
-                        method.invoke(test);
-                    } catch (InvocationTargetException e) {
-                        if (e.getTargetException() instanceof AssertionError) {
-                            failedTests.put((AssertionError) e.getTargetException(), method);
-                        } else {
-                         failedTests.put(new AssertionError(e.getClass().getSimpleName() + " thrown."), method);
-                        }
-                    } catch (IllegalAccessException e) {
-                        //this should just give the user a sane message about where he tried
-                        //to use something private?
-                        e.printStackTrace();
+        for (Method method : testMethods) {
+            if (method.getName().contains("Throws")) {
+                @SuppressWarnings("unchecked")
+                Class<Throwable> err = (Class<Throwable>) method.getParameterTypes()[0];
+                try {
+                    method.invoke(method.getDeclaringClass().newInstance(), err.newInstance());
+                } catch (InvocationTargetException e) {
+                    //noinspection StatementWithEmptyBody
+                    if (!e.getTargetException().getClass().equals(err)) {
+                        failedTests.put(new AssertionError(err.getSimpleName() + " was not thrown"), method);
                     }
+                } catch (Exception e) {
+                    failedTests.put(new AssertionError(e.getMessage()), method);
+                }
+            } else {
+                try {
+                    method.invoke(test);
+                } catch (InvocationTargetException e) {
+                    if (e.getTargetException() instanceof AssertionError) {
+                        failedTests.put((AssertionError) e.getTargetException(), method);
+                    } else {
+                        failedTests.put(new AssertionError(e.getTargetException().toString()), method);
+                    }
+                } catch (IllegalAccessException e) {
+                    //this should just give the user a sane message about where he tried
+                    //to use something private?
+                    failedTests.put(new AssertionError(e.toString()), method);
                 }
             }
         }
     }
 
+    private void printRed(String message) {
+        System.out.printf("%s%s%s", ANSI_RED, message, ANSI_RESET);
+    }
+
+    private void printGreen(String message) {
+        System.out.printf("%s%s%s", ANSI_GREEN, message, ANSI_RESET);
+    }
     private void printResults() {
         int numFailed = testMethods.size() - failedTests.size();
-        System.out.printf("Passed %d com.g01denw01f11.utils.tests of %d\n", numFailed, testMethods.size());
+        printGreen(String.format("Passed %d tests of %d\n", numFailed, testMethods.size()));
         for (AssertionError e : failedTests.keySet()) {
-            System.out.printf("Failed test %s: %s", failedTests.get(e).getName(), e.getMessage());
+            printRed(String.format("Failed test %s: %s\n", failedTests.get(e).getName(), e.getMessage()));
         }
     }
 
-    private List<TestCase> tests = new LinkedList<>();
-    private List<Method> testMethods = new LinkedList<>();
-    private Map<AssertionError, Method> failedTests = new HashMap<>();
 
 }
